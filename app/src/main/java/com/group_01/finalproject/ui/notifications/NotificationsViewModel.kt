ae7d4aa77660b6724a8ce47e2cc2ff660df2897e
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-12-03 14:52:37 -0500

    Initial Commit
    Signed-off-by: DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>

---
+++ app/src/main/java/com/group_01/finalproject/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.group_01.finalproject.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}

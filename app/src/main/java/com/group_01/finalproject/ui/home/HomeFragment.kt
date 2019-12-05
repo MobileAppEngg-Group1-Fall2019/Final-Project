4715a98694756d5fc8e9b5985e6fa8f88d00648a
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-12-05 13:44:06 -0500

    Add home fragment xml functionality
    Signed-off-by: DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>

--- app/src/main/java/com/group_01/finalproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/group_01/finalproject/ui/home/HomeFragment.kt
@@ -19,13 +19,8 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }

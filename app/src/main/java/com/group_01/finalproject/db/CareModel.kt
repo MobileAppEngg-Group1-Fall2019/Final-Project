7a1fa2fe8ff8dcf15f3e356fc57cb65a71144a38
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-12-07 16:43:56 -0500

    Add completed home fragment (points, badges, & history).
    Signed-off-by: DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>

--- app/src/main/java/com/group_01/finalproject/db/CareModel.kt
+++ app/src/main/java/com/group_01/finalproject/db/CareModel.kt
@@ -2,4 +2,10 @@ package com.group_01.finalproject.db
 
 import java.util.*
 
-class CareModel(val careID: Long, val plantID: Long, val date: Date, val caption: String, val completed: Boolean)
+class CareModel(
+    val careID: Long,
+    val plantID: Long,
+    val date: Date,
+    val caption: String,
+    val completed: Boolean
+)
--- app/src/main/java/com/group_01/finalproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/group_01/finalproject/ui/home/HomeFragment.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.group_01.finalproject.R
+import com.group_01.finalproject.db.CareModel
+import com.group_01.finalproject.db.DBInterface
+import com.group_01.finalproject.db.PlantModel
+import com.group_01.finalproject.db.UserModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.history_card.view.*
+import java.util.*
 
+@Suppress("DEPRECATION")
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var dbHelper: DBInterface
+    private var mUser: UserModel? = null
+    private var mCare: ArrayList<CareModel>? = null
+    private var badgesConsistency = listOf<Int>()
+    private var badgesDiversity = listOf<Int>()
+    private var badgesPhotos = listOf<Int>()
+    private var badgesGreenThumb = listOf<Int>()
+    private var badgesOfBadges = listOf<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +35,237 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        dbHelper = DBInterface(context = this.context!!)
+
+//        val testUser = UserModel(
+//            0,
+//            "test_user",
+//            50123,
+//            2,
+//            3,
+//            4,
+//            3,
+//            2,
+//            Date(2018, 10, 1)
+//        )
+//
+//        val testCare = arrayListOf(
+//            CareModel(0,0,Date(2019,8,7),"Caption",true),
+//            CareModel(1,0,Date(2019,19,7),"Caption",false),
+//            CareModel(2,0,Date(2019,10,7),"Caption",true),
+//            CareModel(3,0,Date(2019,11,7),"Caption",true)
+//        )
+//
+//        val testPlant = PlantModel(
+//            0,
+//            "Bob",
+//            "Cactus",
+//            "Alive",
+//            true,
+//            1,
+//            3
+//        )
+
+//        dbHelper.insertUser(testUser)
+//        testCare.forEach {
+//            dbHelper.insertCare(it)
+//        }
+//        dbHelper.insertPlant(testPlant)
+
+        // Get User & Care data from database
+        mUser = dbHelper.getUser(0)
+        mCare = dbHelper.getAllCare()
+
+        createText(); createBadges(); createHistory()
+    }
+
+    // Set TextViews for total points & start date.
+    private fun createText() {
+        val points = mUser?.points.toString()
+        val year = mUser?.creationDate?.year
+        val month = mUser?.creationDate?.month
+        val day = mUser?.creationDate?.day
+
+        val stringMonth = monthToString(month)
+
+        value_total_points.text = "Total Points: $points"
+        value_start_date.text = "Gardening Since $stringMonth $day, $year!"
+    }
+
+    // Create and add image view of badges to badges_layout using user db data.
+    private fun createBadges() {
+        when(mUser?.consistencyBadge){ // CONSISTENCY BADGE  // Range: 0-5
+            1 -> {
+                addBadge(badgesConsistency[0])
+            }
+            2 -> {
+                for (i in 0..1) { addBadge((badgesConsistency[i])) }
+            }
+            3 -> {
+                for (i in 0..2) { addBadge((badgesConsistency[i])) }
+            }
+            4 -> {
+                for (i in 0..3) { addBadge((badgesConsistency[i])) }
+            }
+            5 -> {
+                for (i in 0..4) { addBadge((badgesConsistency[i])) }
+            }
+        }
+        when(mUser?.diversityBadge){ // DIVERSITY BADGE   //Range: 0-6
+            1 -> {
+                addBadge(badgesDiversity[0])
+            }
+            2 -> {
+                for (i in 0..1) { addBadge((badgesDiversity[i])) }
+            }
+            3 -> {
+                for (i in 0..2) { addBadge((badgesDiversity[i])) }
+            }
+            4 -> {
+                for (i in 0..3) { addBadge((badgesDiversity[i])) }
+            }
+            5 -> {
+                for (i in 0..4) { addBadge((badgesDiversity[i])) }
+            }
+            6 -> {
+                for (i in 0..5) { addBadge((badgesDiversity[i])) }
+            }
+        }
+        when(mUser?.photosBadge){ // PHOTOGRAPHER BADGE   //Range: 0-5
+            1 -> {
+                addBadge(badgesPhotos[0])
+            }
+            2 -> {
+                for (i in 0..1) { addBadge((badgesPhotos[i])) }
+            }
+            3 -> {
+                for (i in 0..2) { addBadge((badgesPhotos[i])) }
+            }
+            4 -> {
+                for (i in 0..3) { addBadge((badgesPhotos[i])) }
+            }
+            5 -> {
+                for (i in 0..4) { addBadge((badgesPhotos[i])) }
+            }
+        }
+        when(mUser?.greenThumbBadge){ // GREEN THUMB BADGE   //Range: 0-5
+            1 -> {
+                addBadge(badgesGreenThumb[0])
+            }
+            2 -> {
+                for (i in 0..1) { addBadge((badgesGreenThumb[i])) }
+            }
+            3 -> {
+                for (i in 0..2) { addBadge((badgesGreenThumb[i])) }
+            }
+            4 -> {
+                for (i in 0..3) { addBadge((badgesGreenThumb[i])) }
+            }
+            5 -> {
+                for (i in 0..4) { addBadge((badgesGreenThumb[i])) }
+            }
+        }
+        when(mUser?.badgeOfBadges){ // BADGE OF BADGES   //Range: 0-3
+            1 -> {
+                addBadge(badgesOfBadges[0])
+            }
+            2 -> {
+                for (i in 0..1) { addBadge((badgesOfBadges[i])) }
+            }
+            3 -> {
+                for (i in 0..2) { addBadge((badgesOfBadges[i])) }
+            }
+        }
+    }
+
+    private fun createHistory() {
+        mCare?.forEach {
+            val layoutInflater = LayoutInflater.from(context)
+            val plant = dbHelper.getPlant(it.plantID)
+            val year = it.date.year
+            val month = monthToString(it.date.month)
+            val day = it.date.day
+
+            val card: View = layoutInflater.inflate(
+                R.layout.history_card,
+                history_cards_layout,
+                false
+            )
+
+            card.card_name.text = plant.name
+            card.card_type.text = plant.type
+            card.card_date.text = "$month $day, $year"
+            card.card_bool.text = if(it.completed) "Yes" else "No" // This is ugly but deal with it!
+
+            history_cards_layout.addView(card)
+        }
+    }
+
+    private fun addBadge(badge: Int) {
+        val mImageView = ImageView(context)
+        mImageView.setImageResource(badge)
+        badges_layout.addView(mImageView)
+    }
+
+    private fun monthToString(month: Int?): String {
+        // Month field uses values from 0-11.
+        when (month) {
+            0 -> return "January"
+            1 -> return "February"
+            2 -> return "March"
+            3 -> return "April"
+            4 -> return "May"
+            5 -> return "June"
+            6 -> return "July"
+            7 -> return "August"
+            8 -> return "September"
+            9 -> return "October"
+            10 -> return "November"
+            11 -> return "December"
+        }
+        return ""
+    }
+
+    // Create lists containing references to badge locations
+    init{
+        badgesConsistency = listOf(
+            R.mipmap.badge_consistency_1_foreground,
+            R.mipmap.badge_consistency_10_foreground,
+            R.mipmap.badge_consistency_100_foreground,
+            R.mipmap.badge_consistency_1000_foreground,
+            R.mipmap.badge_consistency_10000_foreground
+        )
+        badgesDiversity = listOf(
+            R.mipmap.badge_diversity_2_foreground,
+            R.mipmap.badge_diversity_4_foreground,
+            R.mipmap.badge_diversity_6_foreground,
+            R.mipmap.badge_diversity_8_foreground,
+            R.mipmap.badge_diversity_10_foreground,
+            R.mipmap.badge_diversity_12_foreground
+        )
+        badgesPhotos = listOf(
+            R.mipmap.badge_photographer_1_foreground,
+            R.mipmap.badge_photographer_10_foreground,
+            R.mipmap.badge_photographer_100_foreground,
+            R.mipmap.badge_photographer_1000_foreground,
+            R.mipmap.badge_photographer_10000_foreground
+        )
+        badgesGreenThumb = listOf(
+            R.mipmap.badge_greenthumb_1_foreground,
+            R.mipmap.badge_greenthumb_10_foreground,
+            R.mipmap.badge_greenthumb_100_foreground,
+            R.mipmap.badge_greenthumb_1000_foreground,
+            R.mipmap.badge_greenthumb_10000_foreground
+        )
+        badgesOfBadges = listOf(
+            R.mipmap.badge_ofbadges_5_foreground,
+            R.mipmap.badge_ofbadges_10_foreground,
+            R.mipmap.badge_ofbadges_20_foreground
+        )
     }
 }

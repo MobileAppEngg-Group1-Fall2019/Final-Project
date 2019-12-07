18d94a6a38e54e469a80cc500c6ce71ac3337889
muhneesh <manishckewalramani@gmail.com>
2019-12-06 21:22:48 -0500

    Commented out the text box in the main activity in the corner, i think it was just there for testing image insertion for the DB added the functionality to the add image button changed the manifest to get a permission for read external storage

--- app/src/main/java/com/group_01/finalproject/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/group_01/finalproject/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package com.group_01.finalproject.ui.notifications
 
+import android.Manifest
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -10,15 +11,31 @@ import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.group_01.finalproject.R
+import com.group_01.finalproject.*
 import com.group_01.finalproject.db.*
 import kotlinx.android.synthetic.main.fragment_notifications.*
+import androidx.core.app.ActivityCompat.startActivityForResult
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import android.R
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.provider.FontsContractCompat.FontRequestCallback.RESULT_OK
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class NotificationsFragment : Fragment() {
 
     var plantNames = arrayListOf<String>()
+    var plantPics = ArrayList<ImageModel>()
     var selectedPlantID : Long = -1
-
+    var SELECT_PHOTO : Int = 12345
 
     private lateinit var notificationsViewModel: NotificationsViewModel
     private lateinit var dbHelper: DBInterface
@@ -30,8 +47,8 @@ class NotificationsFragment : Fragment() {
     ): View? {
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
+        val root = inflater.inflate(com.group_01.finalproject.R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(com.group_01.finalproject.R.id.text_notifications)
         notificationsViewModel.text.observe(this, Observer {
             textView.text = it
         })
@@ -44,26 +61,29 @@ class NotificationsFragment : Fragment() {
 
         dbHelper = DBInterface(context = this.context!!)
 
+
+
         val gridView : GridView = this.photoGrid
 
         val plants: ArrayList<PlantModel> = dbHelper.getAllPlants()
+        plantNames.clear()
         for (plant in plants){
             plantNames.add(plant.name)
         }
         //plantNames.add("*")
         Log.d("Manish testing",plantNames.toString())
 
-        val adapter = ArrayAdapter(this.context,android.R.layout.simple_spinner_item, plantNames)
+        val adapterForSpinner = ArrayAdapter(this.context,android.R.layout.simple_spinner_item, plantNames)
 
         val spinnerPlantNames: Spinner = this.spinner_plantNames
 
-        spinnerPlantNames!!.adapter = adapter
+        spinnerPlantNames!!.adapter = adapterForSpinner
 
         spinnerPlantNames!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
                 //probably do nothing right?
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
 
             }
 
@@ -73,20 +93,88 @@ class NotificationsFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                Log.d("Manish testing", "plant id is " + plants[position].plantId.toString())
-                Log.d("Manish testing","plant name is " + spinnerPlantNames.selectedItem.toString())
-                Log.d("Manish testing", "whatever this id is is " + id.toString())
+                Log.d("Manishtesting", "plant id is " + plants[position].plantId.toString())
+                Log.d("Manishtesting","plant name is " + spinnerPlantNames.selectedItem.toString())
+                Log.d("Manishtesting", "whatever this id is is " + id.toString())
 
                 selectedPlantID = plants[position].plantId
 
-                val plantPics : ArrayList<ImageModel> = dbHelper.getPlantImages(selectedPlantID)
+                plantPics = dbHelper.getPlantImages(selectedPlantID)
 
-                val adapter = PhotoAdapter(context!!, R.layout.photo_grid_element, plantPics)
-                gridView.adapter = adapter
+                val adapterForPhotos = PhotoAdapter(context!!, com.group_01.finalproject.R.layout.photo_grid_element, plantPics)
+                gridView.adapter = adapterForPhotos
             }
 
         }
 
+        val addImageButton : Button = this.addImageButton
+        addImageButton.setOnClickListener{
+
+            Log.d("Manishtesting","pressed the add image button")
+
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+
+            val permission = ContextCompat.checkSelfPermission(context!!,Manifest.permission.READ_EXTERNAL_STORAGE)
+
+            if(permission == PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO)
+            }
+            else{
+                ActivityCompat.requestPermissions(
+                    this.activity!!,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    SELECT_PHOTO
+                )
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO)
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        //&& resultCode == RESULT_OK
+        if (requestCode == SELECT_PHOTO  && data != null) {
+            // Let's read picked image data - its URI
+            val pickedImage = data.data
+            // Let's read picked image path using content resolver
+            val filePath = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor = context!!.contentResolver.query(pickedImage, filePath, null, null, null)
+            cursor.moveToFirst()
+            val imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]))
+
+            val options = BitmapFactory.Options()
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888
+            val bitmap = BitmapFactory.decodeFile(imagePath, options)
+
+            // Do something with the bitmap
+
+            var newImageforDB : ImageModel = ImageModel(-1, selectedPlantID,DbBitmapUtil.getBytes(bitmap),
+                Calendar.getInstance().getTime())
+
+            Log.d("Manishtesting",selectedPlantID.toString())
+
+            dbHelper.insertImage(newImageforDB)
+
+
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close()
+
+            onViewCreated(view!!, Bundle())
+        }
     }
 
 }
+

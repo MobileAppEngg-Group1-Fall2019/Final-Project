744b9f8b99aec67f57326e45612e168b6073be36
sunnyfeng <yfeng1997@gmail.com>
2019-12-07 19:43:29 -0500

    Added dialog alert boxes

--- app/src/main/java/com/group_01/finalproject/ui/dashboard/ListAdapter.kt
+++ app/src/main/java/com/group_01/finalproject/ui/dashboard/ListAdapter.kt
@@ -3,17 +3,19 @@ package com.group_01.finalproject.ui.dashboard
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.group_01.finalproject.R
+import com.group_01.finalproject.db.DBInterface
+import com.group_01.finalproject.db.DbBitmapUtil
+import com.group_01.finalproject.db.PlantModel
+import java.util.*
 
-// Entry data structure to store title and description
-data class Plant(val name: String, val type: String, val indoors: Boolean, val ageInWeeks: Int, val daysBetweenWatering: Int)
-
-class ListAdapter (private val list: ArrayList<Plant>)
+class ListAdapter (private val list: ArrayList<PlantModel>)
     : RecyclerView.Adapter<ViewHolder>() {
 
-    var onItemClick: ((Plant) -> Unit)? = null
+    var onItemClick: ((PlantModel) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -23,10 +25,6 @@ class ListAdapter (private val list: ArrayList<Plant>)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val name = list[position]
         holder.bind(name)
-
-        holder.itemView.setOnClickListener {
-            onItemClick?.invoke(list[position])
-        }
     }
 
     override fun getItemCount(): Int = list.size
@@ -38,20 +36,32 @@ class ViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     private var nameTypeView: TextView? = null
     private var locationAgeView: TextView? = null
     private var waterView: TextView? = null
+    private var plantImageView: ImageView? = null
+    private var dbHelper: DBInterface
 
     init {
         nameTypeView = itemView.findViewById(R.id.plant_name_type)
         locationAgeView = itemView.findViewById(R.id.plant_location_age)
         waterView = itemView.findViewById(R.id.plant_water)
+        plantImageView = itemView.findViewById(R.id.plantImageView)
+
+        dbHelper = DBInterface(context = parent.context)
+
     }
 
-    fun bind(plant: Plant) {
+    fun bind(plant: PlantModel) {
         nameTypeView?.text = plant.name + ", " + plant.type
-        if (plant.indoors) {
-            locationAgeView?.text = "indoors, " + plant.ageInWeeks + " weeks old"
+        if (plant.indoor) {
+            locationAgeView?.text = "indoors, " + plant.age + " weeks old"
         } else {
-            locationAgeView?.text = "outdoors, " + plant.ageInWeeks + " weeks old"
+            locationAgeView?.text = "outdoors, " + plant.age + " weeks old"
         }
-        waterView?.text = "water every " + plant.daysBetweenWatering + " days"
+        waterView?.text = "last watered on " + plant.lastCare.toString()
+
+        var imageList = dbHelper.getPlantImages(plant.plantId)
+        if (imageList.isNotEmpty()) {
+            plantImageView?.setImageBitmap(DbBitmapUtil.getImage(imageList[0].data))
+        }
     }
 }
+

f2f9c7fc32f6b17c5d10ae1b17b76612006b547d
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-12-08 16:24:38 -0500

    Improve Splash Screen.
    Signed-off-by: DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>

--- app/src/main/java/com/group_01/finalproject/SplashActivity.kt
+++ app/src/main/java/com/group_01/finalproject/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.group_01.finalproject
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,10 +13,17 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Remove Action bar and Notification Bar
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_splash)
+
         Handler().postDelayed({
             val mIntent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(mIntent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }, SPLASH_TIME_OUT)
     }

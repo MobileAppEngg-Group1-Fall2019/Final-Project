ab7af3d503c2848792ea6ee1b778117451f1c145
sunnyfeng <yfeng1997@gmail.com>
2019-12-09 16:03:16 -0500

    Added weather notification

--- app/src/main/java/com/group_01/finalproject/NotificationService.kt
+++ app/src/main/java/com/group_01/finalproject/NotificationService.kt
@@ -19,13 +19,14 @@ import kotlin.collections.ArrayList
 
 
 class NotificationService : Service() {
+    val TAG = "##Notification Service"
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
 
-        Log.d("### Service Test", "Notification service running")
+        Log.d(TAG, "Notification service running")
         var dbHelper = DBInterface(this)
         val curTime: Date = Calendar.getInstance().time
 
-        val TAG = "##Notification Service"
         var plants = dbHelper.getAllPlants()
 
         // map of plant to water frequency
@@ -39,7 +40,6 @@ class NotificationService : Service() {
         var user: UserModel = dbHelper.getUser(1)
         var newUser: UserModel = UserModel(user.userId, user.name, user.points + 1, user.consistencyBadge, user.diversityBadge, user.photosBadge, user.greenThumbBadge, user.badgeOfBadges, user.creationDate, user.lat, user.long)
         dbHelper.updateUser(newUser)
-        dbHelper.closeConnection()
         weatherHelper.getCurrentWeatherByGeoCoordinates(user.lat, user.long, object :
             CurrentWeatherCallback {
             override fun onSuccess(currentWeather: CurrentWeather) {
@@ -51,6 +51,8 @@ class NotificationService : Service() {
                             + "Wind Speed: " + currentWeather.wind.speed + "\n"
                             + "City, Country: " + currentWeather.name + ", " + currentWeather.sys.country
                 )
+
+                checkTemperature(currentWeather.main.tempMax)
             }
 
             override fun onFailure(throwable: Throwable) {
@@ -112,6 +114,7 @@ class NotificationService : Service() {
             notificationManager.notify(0, notification)
         }
 
+        dbHelper.closeConnection()
         stopSelf()
 
         return START_NOT_STICKY
@@ -142,77 +145,35 @@ class NotificationService : Service() {
             PendingIntent.getService(this, 0, Intent(this, NotificationService::class.java), 0)
         )
     }
-}
 
+    private fun checkTemperature(temperature: Double) {
 
-/*
-class NotificationService : IntentService("NotificationService") {
-
-    /**
-     * The IntentService calls this method from the default worker thread with
-     * the intent that started the service. When this method returns, IntentService
-     * stops the service, as appropriate.
-     */
-    override fun onHandleIntent(intent: Intent?) {
-        Log.d("### Service Test", "Notification service running")
-        var dbHelper = DBInterface(this)
-        val curTime: Date = Calendar.getInstance().time
-
-        val TAG = "##Notification Service"
-        var plants = dbHelper.getAllPlants()
-
-        // map of plant to water frequency
-        val map = mapOf("Tomato" to 2, "Succulent" to 5 , "Peppers" to 4)
-
-        val plantsToWater = ArrayList<String>()
-        for (plant in plants) {
-            var dateToWater = getWaterDate(plant.lastCare, plant.type, map)
-            if (dateToWater == null) {
-                Log.i(TAG, "Plant type ${plant.type} does not have a corresponding watering frequency.")
-                continue
+        if (temperature < 50) {
+            // Create an Intent for the activity you want to start
+            val resultIntent = Intent(this, MainActivity::class.java)
+            // Create the TaskStackBuilder
+            val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+                // Add the intent, which inflates the back stack
+                addNextIntentWithParentStack(resultIntent)
+                // Get the PendingIntent containing the entire back stack
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
             }
 
-            if(curTime > dateToWater) {
-                Log.i(TAG,"Current Time occurs after Date Water")
-                // Time to water
-                plantsToWater.add(plant.name)
-            } else if(curTime < dateToWater) {
-                Log.i(TAG,"Current Time occurs before Date Water")
-                // No need to water - do nothing
-            } else {
-                Log.i(TAG,"Both dates are equal")
-                // Time to water
-                plantsToWater.add(plant.name)
-            }
-        }
-
-        if (plantsToWater.isEmpty()) {
-            Log.i(TAG, "No plants need to be watered now")
-        } else {
-            Log.i(TAG, "Plants to be watered: $plantsToWater")
-            // send notification
-            val notification = Notification.Builder(this)
-                .setContentTitle("Time to water your plants!")
-                .setContentText("Water: $plantsToWater")
+            val notification = NotificationCompat.Builder(this, "channel_id")
+                .setSmallIcon(R.drawable.notification_flower)
+                .setContentTitle("Warning: Move your outdoor plants inside")
+                .setContentText("Temperature is $temperature, which is unsafe for outdoor plants.")
+                .setContentIntent(resultPendingIntent)
                 .build()
+
+            // Removes notification after it is clicked.
+            notification.flags = notification.flags or Notification.FLAG_AUTO_CANCEL
 
             val notificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             notificationManager.notify(0, notification)
         }
-
     }
+}
 
-    private fun getWaterDate(date: Date, type: String, map: Map<String, Int>): Date? {
-        val calendar = Calendar.getInstance()
-        calendar.time = date
-        val numDays: Int? = map[type]
-        if (numDays != null) {
-            // add number of days
-            calendar.add(Calendar.DAY_OF_YEAR, numDays)
-            return calendar.time
-        }
-        return null
-    }
-} */
